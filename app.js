8feb4db5830b399585532d59fa1060125b00ed66
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-10-04 12:43:37 +0530

    adding check box to check the task

---
+++ app.js
@@ -0,0 +1,10 @@
+// let checkbox = document.querySelector("#task1");
+
+// function toggleStrikethrough(checkbox) {
+//   const label = checkbox.nextElementSibling;
+//   if (checkbox.checked) {
+//     label.classList.add("strikethrough");
+//   } else {
+//     label.classList.remove("strikethrough");
+//   }
+// }
